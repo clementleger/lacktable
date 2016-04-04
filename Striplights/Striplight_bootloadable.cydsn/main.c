c34394a30ac7e0687377c23eab88751f19376866
Clement Leger <clement.leger@kalray.eu>
2016-04-04 21:03:15 +0200

    Add state for blinking led

--- Striplights/Striplight_bootloadable.cydsn/main.c
+++ Striplights/Striplight_bootloadable.cydsn/main.c
@@ -14,10 +14,11 @@
 #define LED_HEIGHT	12
 #define LED_WIDTH	12
 
-#define MIN_SPAWN_TIME		2000
-#define MAX_SPAWN_TIME		5000
-#define STAR_DURATION		6000
-#define LIGHTING_CURVE		500
+#define MIN_SPAWN_TIME		1000
+#define MAX_SPAWN_TIME		4000
+#define STAR_MIN_DURATION	6000
+#define STAR_MAX_DURATION	10000
+#define LIGHTING_CURVE		1000
 
 enum disp_mode {
 	MODE_DRAW = 0,
@@ -55,6 +56,7 @@ randr(unsigned int min, unsigned int max)
 static inline void
 myprintf(const char *fmt , ...)
 {
+#ifndef NDEBUG
 	va_list args;
         char buffer[512];
 
@@ -63,6 +65,7 @@ myprintf(const char *fmt , ...)
         va_end(args);
 
         PC_Uart_UartPutString(buffer);
+#endif
 }
 
 static void
@@ -216,23 +219,40 @@ star_mode()
 	
 }
 
-#define UPDATE_TIME	50
+#define BLINK_UPDATE_TIME		50
+#define BLINK_LIGHTING_INCREMENT	(LIGHTING_CURVE / 50)
+#define BLINK_STEPS			((float) LIGHTING_CURVE / BLINK_LIGHTING_INCREMENT)
 
+struct blink_led_state {
+	uint16_t duration;	/* Led lit duration */
+	uint16_t value;		/* Current duration */
+};
 
 static void
-update_leds (uint16_t state[LED_WIDTH][LED_HEIGHT])
+blink_update_leds (struct blink_led_state state[LED_WIDTH][LED_HEIGHT])
 {
 	int x, y;
-	uint16_t value;
+	uint32_t color;
 	
 	while(StripLights_Ready() == 0);
 	for (x = 0; x < LED_WIDTH; x++) {
 		for (y = 0; y < LED_HEIGHT; y++) {
-			value = state[x][y];
-			if (value != 0) {
-				set_pixel(x, y, hsv_to_rgb(255, 200, 200));
-				state[x][y] -= UPDATE_TIME;
+
+			/* Star is off */
+			if (state[x][y].duration == 0)
+				continue;
+
+			/* Slow lighting curve */
+			if (state[x][y].value < LIGHTING_CURVE) {
+				color = hsv_to_rgb(255, 200, 200)
 			} else {
+				color = hsv_to_rgb(255, 200, 200)
+			}
+
+			set_pixel(x, y, color);
+			state[x][y].value += BLINK_UPDATE_TIME;
+			if (state[x][y].value == state[x][y].duration) {
+				state[x][y].duration = 0;
 				set_pixel(x, y, 0x000000);
 			}
 		}
@@ -241,9 +261,28 @@ update_leds (uint16_t state[LED_WIDTH][LED_HEIGHT])
 }
 
 static void
+blink_spawn_star (struct blink_led_state state[LED_WIDTH][LED_HEIGHT])
+{
+	int x, y;
+	int duration;
+
+	do {
+		x = randr(0, LED_WIDTH - 1);
+		y = randr(0, LED_HEIGHT - 1);
+	} while (state[x][y].duration != 0);
+	
+	duration = randr(STAR_MIN_DURATION, STAR_MAX_DURATION);
+	/* Round it for better sync with update func */
+	duration -= (duration % BLINK_UPDATE_TIME);
+
+	state[x][y].duration = duration;
+	state[x][y].value = 0;
+}
+
+static void
 blink_mode()
 {
-	uint16_t state[LED_WIDTH][LED_HEIGHT] = {{0}};
+	struct blink_led_state state[LED_WIDTH][LED_HEIGHT] = {{0}};
 	uint8_t reg_status;
 	int x, y;
 	uint32_t next_spawn_time = 0, last_spawn_ms = 0, last_update_ms= 0;
@@ -253,19 +292,14 @@ blink_mode()
 	do {
 		/* Spawn a random "star" */
 		if ((ms_count - last_spawn_ms) > next_spawn_time) {
-			do {
-				x = randr(0, LED_WIDTH - 1);
-				y = randr(0, LED_HEIGHT - 1);
-			} while (state[x][y] != 0);
-			state[x][y] = STAR_DURATION;
+			blink_spawn_star(state);
 			next_spawn_time = randr(MIN_SPAWN_TIME, MAX_SPAWN_TIME);
 			last_spawn_ms = ms_count;
-			
 		}
 
 		/* Update every 50 msec */
-		if ((ms_count - last_update_ms) > UPDATE_TIME) {
-			update_leds(state);
+		if ((ms_count - last_update_ms) > BLINK_UPDATE_TIME) {
+			blink_update_leds(state);
 			last_update_ms = ms_count;
 		}
 
@@ -274,7 +308,7 @@ blink_mode()
 		if (!(reg_status & 0x1)) {
 			for (x = 0; x < LED_WIDTH; x++) {
 				for (y = 0; y < LED_HEIGHT; y++) {
-					state[x][y] = 0;
+					state[x][y].duration = 0;
 				}
 			}
 		}
@@ -321,15 +355,16 @@ rainbow_mode()
 struct mode_description {
 	char sign;
 	void (* handler)(void);
+	uint32_t color;
 };
 
 struct mode_description mode_desc[] = 
 {
-	[MODE_DRAW] = { 'D', drawing_mode},
-	[MODE_BLINK] = { 'B', blink_mode},
-	[MODE_SNAKE] = { 'S', star_mode},
-	[MODE_MATRIX] = { 'M', star_mode},
-	[MODE_RAINBOW] = { 'R', rainbow_mode},
+	[MODE_DRAW] = { 'D', drawing_mode, 0xFF9D1D},
+	[MODE_BLINK] = { 'B', blink_mode, 0xE84C27},
+	[MODE_SNAKE] = { 'S', star_mode, 0xFF2AB0},
+	[MODE_MATRIX] = { 'M', star_mode, 0x8827E8},
+	[MODE_RAINBOW] = { 'R', rainbow_mode, 0x2A53FF},
 };
 
 static int
@@ -347,7 +382,7 @@ select_mode()
 
 		if (mode != old_mode) {
 			
-			draw_char(mode_desc[mode].sign, 3, 2, 0x0000FF);
+			draw_char(mode_desc[mode].sign, 3, 2, mode_desc[mode].color);
 			myprintf("Mode change: %s\n", mode_desc[mode].sign);
 		}
 
